035e3906b7091dd34905ac74077a2ca7a162d315
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-07-16 15:33:08 +0000

    A better version of Task#04

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -9,9 +9,6 @@ def add_arrays(arr1, arr2):
     Function to adds two arrays element-wise
     Returns the a new array with the result
     """
-    result = []
     if len(arr1) == len(arr2):
-        for i in range(len(arr1)):
-            result.append(arr1[i] + arr2[i])
-        return result
+        return [arr1[i] + arr2[i] for i in range(len(arr1))]
     return None
