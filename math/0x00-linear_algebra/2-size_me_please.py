5f3f2ad1fe5de1b84c452807b66b622160cda1b9
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-07-16 06:25:37 +0000

    Adding documentation to size_me module

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
-
+"""
+Module with functions to calculate the shape of a Matrix
+"""
 
 def matrix_shape(matrix):
+    """
+    Setup function to call the recursive version
+    Returns the sahpe as a list of integers
+    """    
     shape = []
     return rec_matrix_shape(matrix, shape)
 
 
 def rec_matrix_shape(matrix, shape):
+    """
+    Recursive function to calculate the shape of a Matrix
+    Returns the sahpe as a list of integers
+    """    
     if type(matrix) == list:
         shape.append(len(matrix))
         rec_matrix_shape(matrix[0], shape)
