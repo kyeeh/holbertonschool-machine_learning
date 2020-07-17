0e641f0d802881ee111d9252dcc1065f3236bba0
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-07-17 02:40:15 +0000

    Adding validations fot Task#05

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -9,7 +9,13 @@ def add_matrices2D(mat1, mat2):
     Function to adds two matrices
     Returns the a new matrix with the result
     """
-    if len(mat1) == len(mat2[0]):
-        return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
-                for i in range(len(mat2))]
+    result = []
+    if len(mat1) == len(mat2):
+        for i in range(len(mat1)):
+            if len(mat1[i]) == len(mat2[i]):
+                aux = [mat1[i][j] + mat2[i][j] for j in range(len(mat2[i]))]
+                result.append(aux)
+            else:
+                return None
+        return result            
     return None
