0e69f3335bc388a80bbe78905b6dc13a08d709d2
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-07-17 03:47:45 +0000

    Adding code for Task#{09,10}

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module with function to calculate the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """
+    Function to calculate the shape of a numpy.ndarray
+    Returns the matrix shape
+    """
+    return matrix.shape
