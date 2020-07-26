4804f120217de36ee050759c77f88f34b5068371
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-07-26 06:48:49 +0000

    Adding comments and fixing codestyle

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+"""
+Derivative
+"""
 
 
 def poly_derivative(poly):
+    """
+    Derivative of polynomials
+    """
     drv = [0]
-    if type(poly) == list:
+    if type(poly) == list or len(poly) == 0:
         if len(poly) > 1:
             i = 2
             drv = [poly[1]]
--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+"""
+Series
+"""
 
 
 def summation_i_squared(n):
+    """
+    Sum of i^2
+    """
     if type(n) == int and n > 0:
         return int((n * (n + 1) * (2 * n + 1)) / 6)
     return None
