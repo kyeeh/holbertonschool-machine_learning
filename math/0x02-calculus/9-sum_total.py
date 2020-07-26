60dce67c781007f7c242b1adab9e44350f30110d
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-07-26 16:46:35 +0000

    Updating documentation

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,7 +6,12 @@ Series
 
 def summation_i_squared(n):
     """
-    Sum of i^2
+    Write a function def summation_i_squared(n): that calculates sum_{i=1}^{n} i^2
+    n is the stopping condition
+    Return the integer value of the sum
+    If n is not a valid number, return None
+    You are not allowed to use any loops
+
     """
     if type(n) == int and n > 0:
         return int((n * (n + 1) * (2 * n + 1)) / 6)
