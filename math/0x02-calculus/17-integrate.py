4dbc3d423102a5be72f23c44276a7c5e4706d408
Ricardo Gutiérrez <ricardo@kyeeh.net>
2020-07-26 18:55:31 -0500

    Adding code for polynomial integration

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+from math import trunc
 """
 Integral
 """
+
+
+def check_type(coef):
+    if coef.is_integer():
+        return trunc(coef)
+    return coef
 
 
 def poly_integral(poly, C=0):
@@ -9,23 +16,27 @@ def poly_integral(poly, C=0):
     Calculates the integral of a polynomial:
 
     poly is a list of coefficients representing a polynomial
-        the index of the list represents the power of x that the coefficient belongs to
+        the index of the list represents the power of x that the coefficient
+            belongs to
         Example: if [f(x) = x^3 + 3x +5] , poly is equal to [5, 3, 0, 1]
     C is an integer representing the integration constant
     If a coefficient is a whole number, it should be represented as an integer
     If poly or C are not valid, return None
-    Return a new list of coefficients representing the integral of the polynomial
+    Return a new list of coefficients representing the integral of the
+        polynomial
     The returned list should be as small as possible
 
     """
     if type(poly) == list and len(poly) > 0:
+        itg = []
         if len(poly) > 1:
-            drv = []
             for i in range(1, len(poly)):
                 if isinstance(poly[i], (int, float)):
-                    drv.append(poly[i] * i)
+                    coef = check_type(poly[i - 1] / i)
+                    itg.append(coef)
                 else:
                     return None
-            return drv
-        return [0]
+        coef = check_type(poly[len(poly) - 1] / len(poly))
+        itg.append(coef)
+        return itg
     return None
