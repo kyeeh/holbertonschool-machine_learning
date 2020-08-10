28cea7444dc68ed1c850086b930cc2189e51ab0d
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-08-10 02:17:17 +0000

    Fix Exception type and conditions

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -37,7 +37,7 @@ class DeepNeuralNetwork:
         lys = layers
         randn = np.random.randn
         for i in range(len(lys)):
-            if (type(lys[i]) is not int) and (lys[i] <= 0):
+            if (type(lys[i]) is not int) or (lys[i] <= 0):
                 raise TypeError('layers must be a list of positive integers')
             if i == 0:
                 self.weights['W1'] = randn(lys[i], nx) * np.sqrt(2 / nx)
