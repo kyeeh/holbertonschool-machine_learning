d4449868e8ff1147752fb85076799ad97f212757
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-08-12 02:24:02 +0000

    Validating validations

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -14,8 +14,9 @@ def one_hot_encode(Y, classes):
     Returns: a one-hot matrix (ohm) encoding of Y with shape (classes,m), or
     None on failure
     """
-    if len(Y) <= 0 or (not isinstance(Y, np.ndarray)) or classes < np.amax(Y)\
-            or type(classes) is not int:
+    if len(Y) <= 0 or (not isinstance(Y, np.ndarray)):
+        return None
+    if classes < np.amax(Y) or type(classes) is not int:
         return None
     ax = np.arange(len(Y))
     ohm = np.zeros((classes, len(Y)))
