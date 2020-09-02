e0d81e81fa8d0b6356c73f3ec637dbf29a42f945
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-09-02 03:19:47 +0000

    Fix dropout

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -22,5 +22,6 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         km.add(K.layers.Dense(units=layers[i], activation=activations[i],
                               kernel_regularizer=K.regularizers.l2(lambtha),
                               input_shape=(nx,)))
-    km.add(K.layers.Dropout(1 - keep_prob))
+        if i != (len(layers) - 1):
+            km.add(K.layers.Dropout(1 - keep_prob))
     return km
