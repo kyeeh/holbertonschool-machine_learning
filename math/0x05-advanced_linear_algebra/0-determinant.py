458c5f98416c62b739978a1a2e5104e76c4b6d75
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-10-24 01:58:57 +0000

    Fix validation

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -17,14 +17,14 @@ def determinant(matrix):
 
     Returns: the determinant of matrix
     """
-    if (type(matrix) != list or len(matrix) == 0 or
+    if (type(matrix) is not list or len(matrix) == 0 or
        not all([type(m) == list for m in matrix])):
         raise TypeError('matrix must be a list of lists')
     if len(matrix[0]) != len(matrix):
         raise ValueError("matrix must be a square matrix")
     if matrix == [[]]:
         return 1
-    if len(matrix) == 1 and len(matrix[0]) == 1:
+    if len(matrix) == 1:
         return matrix[0][0]
     if len(matrix) == 2:
         return ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]))
