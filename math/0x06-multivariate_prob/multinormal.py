b05c8d697080dafa1ce5d0fb13fdbee72d6a591a
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-10-27 04:54:30 +0000

    Fix formula for cov on constructor

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -24,13 +24,13 @@ class MultiNormal():
         """
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        if n < 2:
+        if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
         d = data.shape[0]
         n = data.shape[1]
         self.mean = np.mean(data, axis=1).reshape(d, 1)
-        cvrc = data - self.mean
-        self.cov = np.dot(cvrc, cvrc.T) / (n - 1)
+        cov = data - self.mean
+        self.cov = np.dot(cov, cov.T) / (n - 1)
 
     def pdf(self, x):
         """
