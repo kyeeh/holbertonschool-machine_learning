Fix formula for cov on constructor

math/0x06-multivariate_prob/multinormal.py
@@ -29,9 +29,8 @@ class MultiNormal():
         if n < 2:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1).reshape(d, 1)
-        deviation = np.tile(self.mean.reshape(-1), n).reshape(n, d)
-        cov = data.T - deviation
-        self.cov = np.matmul(cov.T, cov)/(n - 1)
+        cov = data - self.mean
+        self.cov = np.dot(cov, cov.T) / (n - 1)
 
     def pdf(self, x):
         """