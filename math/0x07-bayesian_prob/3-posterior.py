059604461930e0c9f92463d8b46656ba40e86a33
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-10-31 02:24:25 +0000

    Fix type validations

--- math/0x07-bayesian_prob/3-posterior.py
+++ math/0x07-bayesian_prob/3-posterior.py
@@ -47,11 +47,9 @@ def posterior(x, n, P, Pr):
     if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.any(P > 1) or np.any(P < 0):
-        raise ValueError(
-            "All values in {} must be in the range [0, 1]".format(P))
+        raise ValueError("All values in P must be in the range [0, 1]")
     if np.any(Pr > 1) or np.any(Pr < 0):
-        raise ValueError(
-            "All values in {} must be in the range [0, 1]".format(Pr))
+        raise ValueError("All values in Pr must be in the range [0, 1]")
     if not np.isclose([np.sum(Pr)], [1])[0]:
         raise ValueError("Pr must sum to 1")
 
