df96bd19dee8b67f19dc0ab46b9fc4c66f17d98f
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-10-31 02:22:20 +0000

    Fix type validations

--- math/0x07-bayesian_prob/100-continuous.py
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -33,15 +33,15 @@ def posterior(x, n, p1, p2):
     """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or (x < 0):
-        mg = "x must be an integer that is greater than or equal to 0"
-        raise ValueError(mg)
+    if not isinstance(x, (int, float)) or (x < 0):
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(p1, float) or p1 < 0 or p1 > 1:
-        raise ValueError("p1 must be a float in the range [0, 1]")
+        raise ValueError("{} must be a float in the range [0, 1]".format(p1))
     if not isinstance(p2, float) or p2 < 0 or p2 > 1:
-        raise ValueError("p2 must be a float in the range [0, 1]")
+        raise ValueError("{} must be a float in the range [0, 1]".format(p2))
     if p2 <= p1:
         raise ValueError("p2 must be greater than p1")
 
--- math/0x07-bayesian_prob/3-posterior.py
+++ math/0x07-bayesian_prob/3-posterior.py
@@ -38,8 +38,8 @@ def posterior(x, n, P, Pr):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or (x < 0):
-        mg = "x must be an integer that is greater than or equal to 0"
-        raise ValueError(mg)
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
@@ -47,9 +47,11 @@ def posterior(x, n, P, Pr):
     if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.any(P > 1) or np.any(P < 0):
-        raise ValueError("All values in P must be in the range [0, 1]")
+        raise ValueError(
+            "All values in {} must be in the range [0, 1]".format(P))
     if np.any(Pr > 1) or np.any(Pr < 0):
-        raise ValueError("All values in Pr must be in the range [0, 1]")
+        raise ValueError(
+            "All values in {} must be in the range [0, 1]".format(Pr))
     if not np.isclose([np.sum(Pr)], [1])[0]:
         raise ValueError("Pr must sum to 1")
 
