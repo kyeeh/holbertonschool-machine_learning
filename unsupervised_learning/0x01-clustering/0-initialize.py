4942c12bcdb9da2213c46626fd2cdc7653ac22c7
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-11-07 04:27:52 +0000

    Add validation for init

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -26,6 +26,13 @@ def initialize(X, k):
     Returns: a numpy.ndarray of shape (k, d) containing the initialized
     centroids for each cluster, or None on failure
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if type(k) != int or k <= 0 or k >= X.shape[0]:
+        return None
+    if type(iterations) != int or iterations <= 0:
+        return None
+
     max = np.max(X, axis=0)
     min = np.min(X, axis=0)
     return np.random.uniform(min, max, size=(k, X.shape[1]))
