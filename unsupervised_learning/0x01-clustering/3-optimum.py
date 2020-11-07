2bb99fa86131036585765e6dfa8f60b3588f8617
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-11-07 04:50:29 +0000

    Rm excp. handler to optium

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -40,15 +40,11 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
-    try:
-        results, d_vars = [], []
-        kmin_var = variance(X, C)
-        for i in range(kmin, kmax + 1):
-            C, klss = kmeans(X, i, iterations)
-            results.append((C, klss))
-            if i == kmin:
-                kmin_var = variance(X, C)
-            d_vars.append(kmin_var - variance(X, C))
-        return (results, d_vars)
-    except Exception:
-        return None, None
+    results, d_vars = [], []
+    for i in range(kmin, kmax + 1):
+        C, klss = kmeans(X, i, iterations)
+        results.append((C, klss))
+        if i == kmin:
+            kmin_var = variance(X, C)
+        d_vars.append(kmin_var - variance(X, C))
+    return (results, d_vars)
