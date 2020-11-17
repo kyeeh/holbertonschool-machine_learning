fe3ed79d2498c2f1eb04401d27a579c83d8648f9
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-11-17 03:38:32 +0000

    Fix validation and codestyle

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -9,19 +9,13 @@ def absorbing(P):
     """
     Determines if a markov chain is absorbing:
 
-    P is a is a square 2D numpy.ndarray of shape (n, n) representing the standard
-    transition matrix
+    P is a is a square 2D numpy.ndarray of shape (n, n) representing the
+    standard transition matrix
         P[i, j] is the probability of transitioning from state i to state j
         n is the number of states in the markov chain
 
     Returns: True if it is absorbing, or False on failure
     """
-    if type(P) is not np.ndarray or len(P.shape) != 2:
-        return False
-    if P.shape[0] != P.shape[1]:
-        return False
-    if np.sum(P, axis=1).all() != 1:
-        return False
     if (np.all(np.diag(P) == 1)):
         return True
     if not np.any(np.diagonal(P) == 1):
